3ce0a5bd5eb29ba8aea62a2824f397f29e562db6
Jeevesh0207 <r27abbit@gmail.com>
2025-02-07 00:08:39 +0530

    `Update CORS configuration in server/index.js`

--- server/index.js
+++ server/index.js
@@ -17,8 +17,9 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["https://taskmanager-ochre-two.vercel.app/"],
-    credentials:true
+    origin: ["https://taskmanager-ochre-two.vercel.app"],
+    credentials:true,
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
@@ -26,6 +27,17 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser());
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://taskmanager-ochre-two.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+  next();
+});
 
 app.use(morgan("dev"));
 
