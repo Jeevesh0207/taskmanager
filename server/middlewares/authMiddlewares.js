fc9d451a598f7c1847de2a24e74a0ec5809d80d0
Jeevesh0207 <r27abbit@gmail.com>
2025-02-07 00:17:51 +0530

    `feat: Update API client to include credentials and server-side changes to handle JWT cookies`

--- server/middlewares/authMiddlewares.js
+++ server/middlewares/authMiddlewares.js
@@ -5,8 +5,6 @@ const protectRoute = async (req, res, next) => {
   try {
     const token = req.cookies?.token;
 
-    console.log(token)
-
     if (token === undefined || token === null) {
       return res
         .status(401)
